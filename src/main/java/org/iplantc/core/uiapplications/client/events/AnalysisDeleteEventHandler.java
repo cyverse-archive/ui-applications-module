1456a4763fe77cb80b2c104586ebc00ebb3e1db0
Sriram Srinivasan <sriram@iplantcollaborative.org>
2012-04-13 10:18:16 -0700

    new event to track analysis delete

---
+++ src/main/java/org/iplantc/core/uiapplications/client/events/AnalysisDeleteEventHandler.java
@@ -0,0 +1,7 @@
+package org.iplantc.core.uiapplications.client.events;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface AnalysisDeleteEventHandler extends EventHandler {
+    public void onDelete(AnalysisDeleteEvent ade);
+}
