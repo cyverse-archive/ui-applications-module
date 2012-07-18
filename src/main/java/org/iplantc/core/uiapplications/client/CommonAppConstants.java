cafcb003bb627c20c9fbe0752e71607653c774fb
Paul Sarando <psarando@iplantcollaborative.org>
2012-07-17 17:28:08 -0700

    CORE-3058 Added AnalysisUtil::setAppNameRegexValidation.
    Added an AnalysisUtil class with a static setAppNameRegexValidation method, for setting regex validation in App Name TextFields.
    Updated CommonAppConstants to extend from class in the ui-commons-module instead of the one in de-common-module.
    Added an ERROR strings instance in the I18N class.

--- src/main/java/org/iplantc/core/uiapplications/client/CommonAppConstants.java
+++ src/main/java/org/iplantc/core/uiapplications/client/CommonAppConstants.java
@@ -1,8 +1,8 @@
 package org.iplantc.core.uiapplications.client;
 
-import org.iplantc.de.client.CommonConstants;
+import org.iplantc.core.uicommons.client.CommonUiConstants;
 
 /** Configurable constants shared by applications */
-public interface CommonAppConstants extends CommonConstants {
+public interface CommonAppConstants extends CommonUiConstants {
     
 }
--- src/main/java/org/iplantc/core/uiapplications/client/I18N.java
+++ src/main/java/org/iplantc/core/uiapplications/client/I18N.java
@@ -1,9 +1,13 @@
 package org.iplantc.core.uiapplications.client;
 
+import org.iplantc.core.uicommons.client.CommonUIErrorStrings;
+
 import com.google.gwt.core.client.GWT;
 
 public class I18N {
     /** Strings displayed in the UI */
     public static final CommonAppDisplayStrings DISPLAY = (CommonAppDisplayStrings)GWT
             .create(CommonAppDisplayStrings.class);
+    public static final CommonUIErrorStrings ERROR = (CommonUIErrorStrings)GWT
+            .create(CommonUIErrorStrings.class);
 }
