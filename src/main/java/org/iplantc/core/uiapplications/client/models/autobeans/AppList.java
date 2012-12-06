5d10f002ec2145af04201d363295c7484933d19a
Jonathan Strootman <jstroot@iplancollaborative.org>
2012-12-06 13:42:28 -0700

    Added App autobean interfaces(cherry picked from commit 034f7cb114645eae0f521378849dafe78dfa7d2f)

---
+++ src/main/java/org/iplantc/core/uiapplications/client/models/autobeans/AppList.java
@@ -0,0 +1,16 @@
+package org.iplantc.core.uiapplications.client.models.autobeans;
+
+import java.util.List;
+
+import com.google.web.bindery.autobean.shared.AutoBean.PropertyName;
+
+/**
+ * This is a convenience class for retrieving JSON arrays of apps
+ * 
+ * @author jstroot
+ * 
+ */
+public interface AppList {
+    @PropertyName("templates")
+    List<App> getApps();
+}
